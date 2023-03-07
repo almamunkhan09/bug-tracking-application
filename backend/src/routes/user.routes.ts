ecf29906f7661c39bd64d4665eb26ab27d9b304a
Al Mamun Khan <almamunkhan09@gmail.com>
2023-03-07 01:45:34 +0100

    Implement create user function

---
+++ backend/src/routes/user.routes.ts
@@ -0,0 +1,18 @@
+import { Request, response, Response, Router } from 'express';
+import { omit } from 'lodash';
+import prisma from '../../prisma';
+import createUser from '../controller/crerateUserHandler';
+import { userValidate } from '../middleWares/userInputValidation';
+
+export const users = Router();
+
+users.get('/', async (req: Request, res: Response) => {
+  try {
+    const allUsers = await prisma.user.findMany();
+    res.status(200).json(allUsers);
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+users.post('/signup', userValidate, createUser);
