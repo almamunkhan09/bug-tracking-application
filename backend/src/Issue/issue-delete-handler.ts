ed14f82547fa88f8d829b19766a835fb4eb51763
Al Mamun Khan <almamunkhan09@gmail.com>
2023-03-28 19:09:15 +0200

    Delete unnecessary logs in issue-delete-handler

--- backend/src/Issue/issue-delete-handler.ts
+++ backend/src/Issue/issue-delete-handler.ts
@@ -3,16 +3,13 @@ import prisma from '../prisma';
 
 export default async function deleteIssue(req: Request, res: Response) {
   const issueId = req.params.issueId;
-  console.log(issueId);
 
   try {
-    console.log(issueId);
     const existingIssue = await prisma.issue.findUnique({
       where: { id: issueId },
     });
-    console.log(existingIssue);
     if (!existingIssue) {
-      res.status(404).json({ error: 'Issue not found' });
+      res.status(404).json({ message: 'Issue not found' });
       return;
     }
 
--- backend/src/routes/apiHealthCheck.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiHealthCheck = void 0;
-const express_1 = require("express");
-exports.apiHealthCheck = (0, express_1.Router)();
-exports.apiHealthCheck.get('/', (req, res) => {
-    return res.sendStatus(200);
-});
